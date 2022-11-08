22150d5f300619818deb4cc64a09ac8667e8985a
Veronica Vybornaya <117581586+wantuta@users.noreply.github.com>
2022-11-08 22:52:41 +0300

    Update B.py

--- HW1/B/B.py
+++ HW1/B/B.py
@@ -1,3 +1,15 @@
 def solution(n):
-    ### YOUR SOLUTION ###
-    return
+    p1 = '   _~_  '
+    p2 = '  (o o)  '
+    p3 = ' /  V  \ '
+    p4 = '/(  _  )\ '
+    p5 = '  ^^ ^^  '
+    print (p1 * n, end = '\n')
+    print (p2 * n, end = '\n')
+    print (p3 * n, end = '\n')
+    print (p4 * n, end = '\n')
+    print (p5 * n, end = '\n')
+    
+
+n = int(input(('How many penguins do you need? ')))
+print(solution(n))
