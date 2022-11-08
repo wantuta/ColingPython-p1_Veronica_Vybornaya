c5a42b4b7e3638827a212988b594cb1a59ca91f5
Veronica Vybornaya <117581586+wantuta@users.noreply.github.com>
2022-11-08 22:09:00 +0300

    Update C.py

--- HW1/C/C.py
+++ HW1/C/C.py
@@ -1,3 +1,15 @@
 def solution(n, k):
-    ### YOUR CODE HERE ###
-    return
+    if k > n:
+        a = k // n
+        b = k - n
+        return(a,b)
+    elif k == n:
+        return(n, 0)
+    else:
+        a = 1
+        b = n - k
+        return(a, b)
+
+n = int(input())
+k = int(input())
+print(solution(n, k))
