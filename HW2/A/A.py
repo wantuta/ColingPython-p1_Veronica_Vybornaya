8140ebfbd7b74ef5548878119a056dec97b59ae6
Veronica Vybornaya <117581586+wantuta@users.noreply.github.com>
2022-12-02 23:22:33 +0300

    Update A.py

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -1,3 +1,10 @@
-def solution(arr):
-    ### YOUR CODE ###
-    return
+def solution(nums):
+        longest_streak=0
+        current=0
+        for i in range(len(nums)):
+            if nums[i-1]==nums[i]:
+                current=current+1
+                longest_streak=max(current, longest_streak)                        
+            else:
+                current=0
+        return longest_streak
