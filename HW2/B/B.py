21b5c3ae621d4204fd71f675b9966dedc456e5ed
Veronica Vybornaya <117581586+wantuta@users.noreply.github.com>
2022-12-03 00:55:55 +0300

    Update B.py

--- HW2/B/B.py
+++ HW2/B/B.py
@@ -1,3 +1,11 @@
 def solution(x):
-    ### YOUR CODE ###
-    return
+    replacement = x.replace('h', 'H', x.count('h') - 1)
+    replacement = replacement.replace('H', 'h', 1)
+    words = list(replacement)
+    for word in words:
+        if word == ' ':
+            words.remove(word)
+    words = words[::3]
+    words = ' '. join(words)
+    words = words.replace('1', 'one')
+    return words
