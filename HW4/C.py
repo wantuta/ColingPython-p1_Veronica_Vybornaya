1bf90019f219a49380a150365833ab2abe6f1f4b
Veronica Vybornaya <117581586+wantuta@users.noreply.github.com>
2022-12-24 18:38:35 +0300

    Update C.py
    Added take function

--- HW4/C.py
+++ HW4/C.py
@@ -3,8 +3,19 @@ def integers():
     while True:
         yield number
         number +=1
-        
+  
+
 def squares():
     int_num = integers()
     for numbers in int_num:
         yield numbers**2
+       
+def take(n, generator):
+    checking = 0
+    num = []
+    for numbers in generator:
+        if checking >= n:
+            break
+        num.append(numbers)
+        checking += 1
+    return num
